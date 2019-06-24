3a2300d53ddfc0158de1d8663ad2a669317450f8
Thamyres Moraes <37299004+thamyresSilva@users.noreply.github.com>
2019-06-24 11:18:19 -0300

    Update get_files_bureau.py

--- get_files_bureau.py
+++ get_files_bureau.py
@@ -1,28 +1,26 @@
 import glob
 from datetime import datetime
-from dateutil.parser import parse
+from dateutil.parser import parse as parse_date
 
 
-quantos = 0
-
-for arquivo in glob.glob('files/*.*'):
-    quantos = quantos + 1
+for arquivo in glob.glob('/home/thamyres/snap/filezilla/17/*.*'):
     a = open(arquivo, 'r')
     caminho_destino = 'processar.txt'
     with open(caminho_destino, 'a+') as arquivo_destino:
-        arquivo_destino.write(f'#{arquivo}')
+        arquivo_destino.write(f'\n #{arquivo} \n')
     
     lines = a.readlines()
+    total_lines = len(lines)
+    for line_number, lines in enumerate(lines):
+        if line_number == 0 or line_number == (total_lines-1):
+            continue
+        else:
+            cpf = lines[1:12]
+            contrato = lines[26:50].strip()
+            data_solicitacao = lines[56:64]
+            data_solicitacao = parse_date(data_solicitacao).strftime('%d/%m/%Y')
+            bureau = lines[51:53]
+            print(cpf, "|", contrato, '|', data_solicitacao, '|', bureau)
 
-    for line in lines:
-        cpf = line[1:12]
-        contrato = line[26:50].strip()
-        data_solicitacao = line[56:64]
-        data_solicitacao = parse(data_solicitacao)
-        bureau = line[51:53]
-        print(cpf, "|", contrato, '|', data_solicitacao, '|', bureau)
-       
-        with open(caminho_destino, 'a+') as arquivo_destino:
-            arquivo_destino.write(f'|{cpf}|{contrato}|{data_solicitacao}|{bureau}|')
-
-    print('Quantidade de arquivos: ', quantos)
+            with open(caminho_destino, 'a+') as arquivo_destino:
+                arquivo_destino.write(f'|{cpf}|{contrato}|{data_solicitacao}|{bureau}| \n')
